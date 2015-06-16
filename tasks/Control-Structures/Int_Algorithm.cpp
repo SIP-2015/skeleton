c4ac385312ed81b9ad353c2bae90bef6341c86fb
Jessica Sizemore <jsize0811@gmail.com>
2015-06-16 16:31:23 -0400

    Create Int_Algorithm.cpp

---
+++ tasks/Control-Structures/Int_Algorithm.cpp
@@ -0,0 +1,33 @@
+/*Design an algorithm that will read a series of integers 
+ *from the screen. The first integer is special, as it 
+ *indicates how many integers will follow. Your algorithm 
+ *is to calculate and print the sum and average of the integers, 
+ *excluding the first integer and display these values to the screen. 
+
+ DISPLAY: 
+ 
+  Enter the number of entries --> 10
+  Enter entry # 1 --> 10
+  Enter entry # 2 --> 65
+  Enter entry # 3 --> 77
+  Enter entry # 4 --> 31
+  Enter entry # 5 --> 2
+  Enter entry # 6 --> 13
+  Enter entry # 7 --> 15
+  Enter entry # 8 --> 24
+  Enter entry # 9 --> 92
+  Enter entry # 10 --> 77
+  The sum = 406 and Average = 40.6
+
+  HINT: Set your values as doubles.
+*/
+
+#include <iostream>
+
+ using namespace std;
+
+int main() {
+
+  // YOUR CODE HERE
+
+}
