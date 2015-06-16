fe15881098321561ebf8c229f1df223cfa2b1891
Jessica Sizemore <jsize0811@gmail.com>
2015-06-16 16:30:44 -0400

    Create Loop _Total_Payroll.cpp

---
+++ tasks/Control-Structures/Loop _Total_Payroll.cpp
@@ -0,0 +1,24 @@
+/*Design an algorithm that will prompt for, receive, 
+ *and total a collection of payroll amounts entered at 
+ *the terminal until a sentinel amount of zero is entered. 
+ *After the sentinel has been entered, display the total 
+ *payroll amount to the screen.
+
+ Enter next pay (0 to quit) --> 600
+ Enter next pay (0 to quit) --> 720
+ Enter next pay (0 to quit) --> 50
+ Enter next pay (0 to quit) --> 633
+ Enter next pay (0 to quit) --> 999
+ Total payroll was: $2003
+
+*/
+
+#include <iostream>
+
+ using namespace std;
+
+ int main() {
+
+ 	// YOUR CODE HERE
+}
+
