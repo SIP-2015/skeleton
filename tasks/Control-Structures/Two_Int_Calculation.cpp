993986bf095eec5d546a1961105ade9f20b37d2b
Jessica Sizemore <jsize0811@gmail.com>
2015-06-16 16:30:15 -0400

    Create Two_Int_Calculation.cpp

---
+++ tasks/Control-Structures/Two_Int_Calculation.cpp
@@ -0,0 +1,28 @@
+/*Design an algorithm that will receive input of 
+ *two integers and display to the screen their sum, 
+ *difference, product, and quotient.  
+ *DO NOT allow division by zero to occur.
+ *Display an error statment when the second integer is Zero.
+
+ 15 + 0 = 15
+ 15 - 0 = 15
+ 15 * 0 = 0
+ ERROR: Division by zero is NOT allowed.
+
+ 6 + 8 = 14
+ 6 - 8 = -2
+ 6 * 8 = 48
+ 6 / 8 = 0.75
+
+HINT: Set your number variables to double.
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+   
+   // YOUR CODE HERE
+}
