5d18fe097d1fcb7d21c5a970f8c9046c89f0b8d0
Jessica Sizemore <jsize0811@gmail.com>
2015-06-16 16:28:47 -0400

    Create Swap_Int.cpp

---
+++ tasks/Control-Structures/Swap_Int.cpp
@@ -0,0 +1,53 @@
+/* Please write a program that inputs two intergers
+ * and then swaps them if the First Interger is 
+ * larger then the Second Interger.
+
+ *Display:
+
+ Lowest: 3
+ Highest: 6
+ No Swap Needed!!
+
+ *Display:
+
+ Lowest: 15
+ Highest: 25
+ Nice Job Swapping!!
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {/* Please write a program that inputs two intergers
+ * and then swaps them if the First Interger is 
+ * larger then the Second Interger.
+
+ *Display:
+
+ Lowest: 3
+ Highest: 6
+ No Swap Needed!!
+
+ *Display:
+
+ Lowest: 15
+ Highest: 25
+ Nice Job Swapping!!
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+	// YOUR CODE HERE
+
+}
+
+
+
+}
