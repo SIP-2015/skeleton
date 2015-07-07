bec931570d4964815a1ec3fd14242ca7f6a5864c
Jessica Sizemore <jsize0811@gmail.com>
2015-07-07 16:02:22 -0400

    Create Sort_Three.cpp

---
+++ tasks/Arguments/Sort_Three.cpp
@@ -0,0 +1,27 @@
+/*
+ *
+ *
+ *	Display:
+ *
+ * 			Enter three integers seperated by a space: 34 628 27
+ *			Sorted: 27, 34, 628
+ *
+ *
+ *
+ */
+
+
+
+
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+void sort(int& x, int& y);
+
+int main (){
+
+	//YOUR CODE HERE
+}
