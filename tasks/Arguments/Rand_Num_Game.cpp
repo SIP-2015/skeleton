50cd08c60144dc04b96806f8c4a138d5370b581e
Jessica Sizemore <jsize0811@gmail.com>
2015-07-07 16:01:48 -0400

    Create Rand_Num_Game.cpp

---
+++ tasks/Arguments/Rand_Num_Game.cpp
@@ -0,0 +1,68 @@
+/*
+ *
+ *
+ *
+ * 	Display:
+ *
+ *			Welcome to the Guessing Game.
+ *		I have a secret number between 1 and 100. CAN YOU GUESS IT??
+ *
+ *		Please enter a guess: 56
+ *		Too High.
+ *		Please enter a guess: 35
+ *		Too High.
+ *		Please enter a guess: 24
+ *		Too Low.
+ *		Please enter a guess: 25
+ *		Too Low.
+ *		Please enter a guess: 26
+ *
+ *
+ *		Correct!! You Win!!
+ *		The secret number was: 26 and you guessed it in 5 turns.
+ *
+ *
+ */
+
+
+
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+void playGame();
+void getRandomNumber(int& number);
+void getUserGuess(int& guess);
+string evaluateGuess(int secret, int guess);
+
+int main (){
+
+	srand((unsigned)time(0));
+	int secret = rand() % 100 +1;
+
+	playGame();
+
+}
+
+void playGame()
+{
+	// YOUR CODE HERE
+}
+
+void getRandomNumber(int& number)
+{
+	number = rand() % 100 + 1;
+}
+
+void getUserGuess (int& guess)
+{
+	// YOUR CODE HERE
+}
+
+string evaluateGuess(int secret, int guess)
+{
+	// YOUR CODE HERE
+}
