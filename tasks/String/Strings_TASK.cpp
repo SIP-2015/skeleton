1568fc227a132f75fabe72627e912475c7ad94f0
Jessica Sizemore <jsize0811@gmail.com>
2015-07-10 11:36:15 -0400

    Create Strings_TASK.cpp

---
+++ tasks/String/Strings_TASK.cpp
@@ -0,0 +1,27 @@
+/* Write a program that reads a string and does the following: 
+ *
+ * - The number of characters in the string.
+ * - The first ten characters in the string. 
+ * - The last ten characters in the string. // Hint: There are two ways to find this
+ * - The last ten characters in the string.
+ * - What is the index of the FIRST space in the string?
+ * - What is the index of the SECOND space in the string?
+ * - What is the index where 'we' is found in the string?
+ * - Replace 'Jessica' with 'Kenny'.
+ * - Replace 'Kenny' with 'Jessica'.
+ * - Insert 'not' after the word 'is'.
+ * - Erase the word 'way' from the string.
+ *
+ *
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	
+	string test = "Jessica is way more awesome than Kenny.";
+
+          
+}
