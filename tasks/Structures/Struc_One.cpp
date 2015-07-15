d28319db41b2b7f7089b48bae38a0a47d9550e8c
Jessica Sizemore <jsize0811@gmail.com>
2015-07-15 08:25:25 -0400

    Create Struc_One.cpp

---
+++ tasks/Structures/Struc_One.cpp
@@ -0,0 +1,33 @@
+/* A point on the two-dimensional plane can be represented by 
+ * two numbers: an x coordinate and a y coordinate. For
+ * example, (4, 5) represents a 4 units to the right of the 
+ * vertical axis, and 5 units up the horizontal axis. The sum
+ * of two points can be defined as a new point whose x coordinate 
+ * is the sum of the x coordinate of the two points, and whose y
+ * coordinate is the sum of the y coordinate.
+ *
+ * Write a programe that uses a structure called POINT to model 
+ * a point. Definre three points, and have the user input values
+ * to two of them. The set third point equal to the sum of the 
+ * other two, and display the value o the new point. Interaction
+ * with the program might look like this:
+ *
+ *
+ * 		Enter coordinates for p1 separated by a space: 5 6
+ *
+ * 		Enter coordinates for p2 separated by a space: 5 6
+ *
+ * 		Coordinates of p1+p2 are: 10, 12
+ *
+ */
+
+ #include <iostream>
+
+ using namespace std;
+
+
+int main () {
+
+	// YOUR CODE HERE
+
+}
