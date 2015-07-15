ae369b66556686d69ce37cdf4b7259036321ceb7
Jessica Sizemore <jsize0811@gmail.com>
2015-07-15 08:25:54 -0400

    Create Struc_Two.cpp

---
+++ tasks/Structures/Struc_Two.cpp
@@ -0,0 +1,21 @@
+/* A phone number, such ar (212)-767-8900, can be thought of as having 
+ * ONE. Initialize one,and have the user input a number
+ * for the other one. Then display both numbers. The interchange might looks
+ * like this: 
+ *
+ * 		Enter your area code, exchange, and number.
+ * 		(XXX XXX XXXX): 240 298 9632
+ *
+ * 		My number is (212) 767 - 8900
+ * 		Your number is (240) 298 - 9632
+ *
+ */
+
+ #include <iostream>
+
+ using namespace std;
+
+ int main (){
+
+ 	// YOUR CODE HERE
+ }
