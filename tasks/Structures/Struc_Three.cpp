0d137b30b9d69bbc7ca8aa78660d495e6095881d
Jessica Sizemore <jsize0811@gmail.com>
2015-07-15 08:26:26 -0400

    Create Struc_Three.cpp

---
+++ tasks/Structures/Struc_Three.cpp
@@ -0,0 +1,20 @@
+/* Use a structure to model a playing card. The program imitatws a game played by a cardsharps at carnivals. 
+ * The cardsharp shows three cards, then places them face down on the table and intershange their positioin 
+ * several times. If you can guess correctly where a particular card is, you win.  
+ * 
+ *
+ * 		Hint: The NUMBER runs from 2 to 14, where 11, 12, 13 and 14 represent the Jack, Queen King and Ace.
+ *			  The SUIT runs from 0 to 3, where these four numbers represents Clubs, Diamonds, Hearts and Spades.
+ *
+ *
+ */
+
+ #include <iostream>
+
+
+ using namespace std;
+
+ int main () {
+
+ 	// YOUR CODE HERE
+ }
